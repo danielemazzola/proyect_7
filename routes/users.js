90a5307122437e5309ef93ef2e3a08bc67c61ac8
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-05-20 23:15:35 +0200

    solution messages

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const ROUTER = express.Router()
+const { AuthUser } = require('../middleware/AuthUser')
 const AllUsers = require('../controllers/users/AllUsers')
 const Register = require('../controllers/users/Register')
 const Update = require('../controllers/users/Update')
@@ -7,7 +8,7 @@ const Login = require('../controllers/users/Login')
 
 ROUTER.get('/', AllUsers)
 ROUTER.post('/register', Register)
-ROUTER.put('/update/:_id', Update)
+ROUTER.put('/update/:_id', AuthUser, Update)
 ROUTER.post('/login', Login)
 
 module.exports = ROUTER
